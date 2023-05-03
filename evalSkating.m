c269351bd86eeb2d4a932a48a0694dbcc1e0511e
Kim Joris Boström <kim.bostroem@predimo.com>
2023-05-03 12:00:10 +0200

    Fix COP and calc distance to beam

--- evalSkating.m
+++ evalSkating.m
@@ -6,113 +6,138 @@ outDir = fullfile('..', 'Skating_Out');
 dirInfo = dir(inDir);
 fdirs = {dirInfo.folder}';
 fnames = {dirInfo.name}';
-idx = startsWith(fnames', {'.', '~'}) | [dirInfo.isdir];
+idxContact = startsWith(fnames', {'.', '~'}) | [dirInfo.isdir];
 fpaths = strcat(fdirs, filesep, fnames);
-fpaths(idx) = []; % remove folders and hidden files
+fpaths(idxContact) = []; % remove folders and hidden files
 nObs = length(fpaths);
-nObs = 3;
+nObs = 1;
 observations = table;
 for iObs = 1:nObs
-   observation = table;
-   fpath = fpaths{iObs};
-   [~, fname, ~] = fileparts(fpath);
-   if contains(fname, 'ungueltig', 'IgnoreCase', true)
-       continue
-   end
-   parts = strsplit(fname, '_');   
-   observation.subject = string(parts{1});   
-   observation.task = string(parts{2});   
-   observation.side = 'B';
-   observation.time = 1;
-   observation.trial = str2double(parts{end});
-   if length(parts) > 3
-       if strcmp(parts{3}, 'Post')
-           observation.time = 2;
-           observation.side = parts{4};
-       else
-           observation.time = 1;
-           observation.side = string(parts{3});
-       end
-   end
+    observation = table;
+    fpath = fpaths{iObs};
+    [~, fname, ~] = fileparts(fpath);
+    if contains(fname, 'ungueltig', 'IgnoreCase', true)
+        continue
+    end
+    parts = strsplit(fname, '_');
+    observation.subject = string(parts{1});
+    observation.task = string(parts{2});
+    observation.side = 'B';
+    observation.time = 1;
+    observation.trial = str2double(parts{end});
+    if length(parts) > 3
+        if strcmp(parts{3}, 'Post')
+            observation.time = 2;
+            observation.side = parts{4};
+        else
+            observation.time = 1;
+            observation.side = string(parts{3});
+        end
+    end
 
-   tmp = load(fpath);
-   fields = fieldnames(tmp);
-   Data = tmp.(fields{1});
-   lengthScale = 0.001; % mm -> m
-   % [Forces, ~, COP0, ~, Location, Analog] = kraftAusQualisys(Data, lengthScale);   
-   [Forces, Frequency, COPs, ~, Location, Analog] = kraftAusQualisys(Data, lengthScale);
-   % COPs = COPs + squeeze(Location(:, 4, :));
-   [COPs, ~, Forces, Loaded] = getCOPfromAnalog(Analog, Forces, [], Location, Frequency, [], [], [], COPs);
-   % ignore data if Force in Z -direction is smaller than -LoadThresh Newton
-   LoadThresh0 = 200;
-   HumPeriod = Frequency/50;
-   CutoffFrequency = 20;
-   nPlates = size(Forces, 1);
-   nDims = size(Forces, 2);
-   nSamples = size(Forces, 3);
-   Force = zeros(3, nSamples);
-   COP = zeros(3, nSamples);
-   Time = (0:nSamples-1)/Frequency;
-   COP = squeeze(sum(COPs, 1, 'omitnan'));
-   Force = squeeze(sum(Forces, 1, 'omitnan'));
-   % for iPlate = 1:nPlates
-   %     if min(Forces(iPlate, 3, :), [], 'all') < -LoadThresh0
-   %     % if Loaded(plate)
-   %         % Loaded(plate) = true;
-   %         % Force_filt   = squeeze(Forces(iPlate, :, :));
-   %         % COP_filt   = squeeze(COPs(iPlate, :, :));           
-   %         % % 50 Hz Netzbrumm entfernen
-   %         % for iDim = 1:nDims
-   %         %     Force_filt(iDim, :) = periodicMedianFilter(Force_filt(iDim, :), HumPeriod);
-   %         %     COP_filt(iDim, :) = periodicMedianFilter(COP_filt(iDim, :), HumPeriod);
-   %         % end
-   %         % Glaetten
-   %         % Force_filt = nanfilth(CutoffFrequency, Frequency, 2, Force_filt, 'l', 2);
-   %         Force    = Force + Force_filt;
-   %         % COP_filt = nanfilth(CutoffFrequency, Frequency, 2, COP_filt, 'l', 2);
-   %         COP    = COP + COP_filt;
-   %     end
-   % end
-   Force = -Force; % force -> reaction force
-   % COP = [-COP(1, :); -COP(2, :); COP(3, :)]; % rotate 180° on XY-plane to improve readability (negative values -> positive)
+    tmp = load(fpath);
+    fields = fieldnames(tmp);
+    Data = tmp.(fields{1});
+    lengthScale = 0.001; % mm -> m
+    [Forces, Frequency, COPs, ~, Location, Analog] = kraftAusQualisys(Data, lengthScale);
+    Forces = -Forces; % ground reaction forces are inverse of plate forces
+    nPlates = size(Forces, 1);
+    nSamples = size(Forces, 3);
+    LoadThresh = 20;
+    HumPeriod = Frequency/50;
+    CutoffFrequency = 20;
+    Time = (0:nSamples-1)/Frequency;
+    for iPlate = 1:nPlates
+        Forces(iPlate, :, :) = periodicMedianFilter(squeeze(Forces(iPlate, :, :)), HumPeriod);
+    end
+    [COPs, ~, Forces, Loaded] = getCOPfromAnalog(Analog, Forces, [], Location, Frequency, [], [], [], COPs);
+    % ignore data if Force in Z -direction is smaller than -LoadThresh Newton
+    for iPlate = 1:nPlates
+        idxContact = (abs(Forces(iPlate, 3, :)) < LoadThresh);
+        Forces(iPlate, :, idxContact) = 0;
+        COPs(iPlate, :, idxContact) = NaN;
+    end
+    Force = squeeze(sum(Forces, 1, 'omitnan'));
+    absForces = abs(Forces(:, 3, :)); % z-component of force
+    weightedCOPs = (absForces .* COPs) ./ sum(absForces, 1);
+    COP = squeeze(sum(weightedCOPs, 1, 'omitnan'));
+    % remove non-contact phases
+    idxContact = ~any(COP, 1);
+    COP(:, idxContact) = NaN; 
+    idxContact = ~any(isnan(COP), 1);
+    nSamplesContact = sum(idxContact);
 
-   switch observation.task
-       case 'Einbein'
-       case 'Balance'
-       case 'Sprung'
-       otherwise
-           continue
-   end
-   % path length
-   dt = median(diff(Time));
-   T = Time(end) - Time(1);
-   pathLength = sum(vecnorm(diff(COP, 1, 2), 2, 1), 2)/T;
-
-   precision = sqrt(sum(var(COP, [], 2)));
+    switch observation.task
+        case 'Einbein'
+        case 'Balance'
+            % path length            
+            pathLength = sum(vecnorm(diff(COP, 1, 2), 2, 1), 2, 'omitnan')/sum(diff(Time));
+            precision = pathLength;
+            COPx = COP(1, idxContact);
+            COPy = COP(2, idxContact);
+            coefficients = polyfit(COPx, COPy, 1);            
+            yBeamFcn = @(x) polyval(coefficients, x);
+            xFit = linspace(min(COPx), max(COPx), nSamplesContact);
+            yFit = yBeamFcn(xFit);
+            beamDistFcn = @(x, y) abs(y - yBeamFcn(x));
+            beamDist = beamDistFcn(COPx, COPy);
+        case 'Sprung'
+        otherwise
+            continue
+    end
 
 
-   if doPlot
-       fig = setupFigure(800, 600, fname);
-       subplot(2, 1, 1);       
-       plot(Time', COP');
-       title(sprintf('%s COP', fname), 'Interpreter', 'none');
-       legend({'x', 'y', 'z'});
-       xlabel('Time [s]');
-       ylabel('Position [m]');
-       xlim([Time(1), Time(end)]);
-       subplot(2, 1, 2);       
-       plot(Time', Force');
-       title(sprintf('%s GRF', fname), 'Interpreter', 'none');
-       legend({'x', 'y', 'z'});
-       xlabel('Time [s]');
-       ylabel('Force [N]');
-       xlim([Time(1), Time(end)]);
-       filePath = fullfile(outDir, fname);
-       saveFigure(fig, filePath, 'png');
-       close(fig);
-   end
+    if doPlot
+        nRows = 4;
+        nCols = 1;
+        iPlot = 0;
+        fig = setupFigure(nCols*800, nRows*200, fname);
 
-   observations = [observations; observation]; %#ok<AGROW>
+        % plot COP path
+        iPlot = iPlot+1;        
+        subplot(nRows, nCols, iPlot);
+        scatter(COP(1, :), COP(2, :), 'bo');
+        title(sprintf('COP path of "%s"', fname), 'Interpreter', 'none');
+        xlabel('x [m]');
+        ylabel('y [m]');
+        hold on
+        scatter(COP(1, :), yBeamFcn(COP(1, :)), '.r');
+        axis equal
+        
+        % plot COP components
+        iPlot = iPlot+1;        
+        subplot(nRows, nCols, iPlot);
+        plot(Time', COP');
+        xlim([Time(1), Time(end)]);
+        title(sprintf('COP components'), 'Interpreter', 'none');
+        xlabel('Time [s]');
+        ylabel('Position [m]');
+        legend({'x', 'y', 'z'});
+        
+        % plot distance to beam
+        iPlot = iPlot+1;
+        subplot(nRows, nCols, iPlot);
+        plot(Time', beamDistFcn(COP(1, :), COP(2, :))');
+        xlim([Time(1), Time(end)]);
+        title(sprintf('Distance to beam'), 'Interpreter', 'none');
+        xlabel('Time [s]');
+        ylabel('Distance [m]');
+
+        % plot GRF
+        iPlot = iPlot+1;
+        subplot(nRows, nCols, iPlot);
+        plot(Time', Force');
+        title(sprintf('Ground reaction force'), 'Interpreter', 'none');
+        legend({'x', 'y', 'z'});
+        xlabel('Time [s]');
+        ylabel('Force [N]');
+        xlim([Time(1), Time(end)]);
+        filePath = fullfile(outDir, fname);
+        saveFigure(fig, filePath, 'png');
+        close(fig);
+    end
+
+    observations = [observations; observation]; %#ok<AGROW>
 end
 
 subjects = unique(observations.subject);
