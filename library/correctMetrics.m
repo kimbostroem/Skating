dc83ab83962682374633e81900c776832f43d7ac
Kim Joris Boström <kim.bostroem@predimo.com>
2023-05-26 11:42:38 +0200

    Add script correctMetrics

---
+++ library/correctMetrics.m
@@ -0,0 +1,71 @@
+function correctMetrics
+
+% load Measurements structure
+load('Measurements.mat'); %#ok<LOAD>
+
+nMeas = length(Measurements.Observations); %#ok<NODEF>
+outDir = '.';
+
+fprintf('Correct metrics...\n');
+for iMeas = 1:nMeas
+    fileName = Measurements.Observations(iMeas).fileName;
+
+    % if metrics not yet calculated -> skip
+    if ~Measurements.Observations(iMeas).doneMetrics
+        Measurements.Observations(iMeas).isValid = 0;
+        continue
+    end
+
+    % report progress
+    fprintf('\t-> %s (%d/%d = %.0f%%)\n', fileName, iMeas, nMeas, iMeas/nMeas*100);
+
+    % get variables
+    task = Measurements.Observations(iMeas).task;
+    COP = Measurements.Data(iMeas).COP;
+    idxContact = Measurements.Data(iMeas).idxContact;
+    stopPos = Measurements.Data(iMeas).stopPos;
+    pathLength = Measurements.Observations(iMeas).pathLength;
+
+    % mark invalid datasets
+    Measurements.Observations(iMeas).isValid = 1;
+    if pathLength <= 0 || isnan(pathLength) % something is very wrong
+        Measurements.Observations(iMeas).isValid = 0;
+        Measurements.Observations(iMeas).pathLength = NaN;
+        Measurements.Observations(iMeas).targetError = NaN;
+        Measurements.Observations(iMeas).meanJerk = NaN;
+        Measurements.Observations(iMeas).meanJerkXY = NaN;
+    end
+
+    % get target error
+    switch task
+        case 'Balance'
+            % distance to beam (for task "Balance")
+            coefficients = polyfit(COP(1, idxContact), COP(2, idxContact), 1);
+            targetFcn = @(x, y) polyval(coefficients, x);
+            targetDistFcn = @(x, y) abs(y - targetFcn(x, y));
+            targetDist = targetDistFcn(COP(1, :), COP(2, :));
+            targetError = mean(targetDist, 'omitnan');
+        case 'Einbein'
+            meanCOP = mean(COP, 2, 'omitnan');
+            targetDistFcn = @(x, y) vecnorm([x; y] - meanCOP);
+            targetDist = targetDistFcn(COP(1, :), COP(2, :));
+            targetError = mean(targetDist, 'omitnan');
+        case 'Sprung'
+            targetDist = abs(COP(1, :) - stopPos(1));
+            [targetError, ~] = min(targetDist);            
+    end
+    % store corrected variables
+    Measurements.Data(iMeas).targetDist = targetDist;
+    Measurements.Observations(iMeas).targetError = targetError;
+end
+
+% save Measurements structure to MAT file
+fprintf('\t\t- Saving Measurements structure to MAT file...\n');
+save(fullfile(outDir, 'Measurements.mat'), 'Measurements');
+
+fprintf('\t\t- Saving Observations to table...\n');
+MeasurementTable = struct2table(Measurements.Observations);
+outpath = fullfile(outDir, 'Observations.xlsx');
+writetable(MeasurementTable, outpath, 'WriteMode', 'replacefile');
+
+end
