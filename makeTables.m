7df0bdc6893015164452f1631bbef328e69374fb
Kim Joris Boström <kim.bostroem@predimo.com>
2023-05-31 13:24:46 +0200

    makeTables
    Now working. Also Added new function saveTable

--- makeTables.m
+++ makeTables.m
@@ -5,62 +5,161 @@ init
 % load current state
 loadState;
 
-nMeas = length(Measurements.Observations); 
 outDir = strrep(Measurements.outDir, '\', '/');
 
-Observations = struct2table(Measurements.Observations);
+OrigTable = struct2table(Measurements.Observations);
 
-fields = {'subject', 'stage', 'adhs', 'medication', 'height', 'weight', 'age', 'intervention', 'task', 'side', 'pathLength', 'targetError', 'meanJerk', 'meanJerkXY', 'trial'};
-
-subjectProps = {'subject', 'adhs', 'height', 'weight', 'age'};
-conditions = {'stage', 'task', 'side'};
+conditions = {'stage', 'task'};
 depVars = {'pathLength', 'targetError', 'meanJerk', 'meanJerkXY'};
-trialVar = 'trial';
+subjects = unique([Measurements.Observations.subject]);
 
-subjects = {Measurements.Observations.subject};
+%% create clean table
 
+SourceTable = OrigTable;
+% remove empty "isValid" rows
+rows = ~cellfun(@isempty,SourceTable.isValid);
+SourceTable = SourceTable(rows, :);
+% remove rows where "isValid" equals zero
+rows =  (cell2mat(SourceTable.isValid) == 1);
+SourceTable = SourceTable(rows, :);
+CleanTable = SourceTable;
+
+%% create long mean table
+
+SourceTable = CleanTable;
+LongTable = table;
 for iSubject = 1:length(subjects)
     subject = subjects{iSubject};
-    subjectTable = Observations(Observations.subject == string(subject), :);  
-    levels = cell(1, length(conditions));    
+    subjectTable = SourceTable(SourceTable.subject == string(subject), :);
+    levels = cell(1, length(conditions));
     for iCond = 1:length(conditions)
         condition = conditions{iCond};
         levels{iCond} = unique(subjectTable.(condition));
-        for iLevel = 1:length(levels{iCond})
-            level = levels{iCond}(iLevel);            
-            levelsStrs{iCond}(iLevel) = string(condition) + " == " + level;
-        end
     end
-    condCombs = string(table2cell(combinations(levelsStrs{:})));
+    condCombs = table2cell(combinations(levels{:}));
     nCondCombs = size(condCombs, 1);
-    condStrs = strings(nCondCombs, 1);    
-    for iStr = 1:nCondCombs
-        myTable = subjectTable;
-        for iBla = 1:length(condCombs(iStr, :))
-            parts = strsplit(condCombs(iStr, iBla));
-            [value, isNum] = str2num(parts{3});
-            if isNum
-                rows = (myTable.(parts{1}) == value);
-            else
-                rows = (myTable.(parts{1}) == parts{3});
+    for iComb = 1:nCondCombs
+        combTable = subjectTable;
+        for iCond = 1:length(condCombs(iComb, :))
+            cond = conditions{iCond};
+            value = condCombs{iComb, iCond};
+            rows = (combTable.(cond) == value);
+            combTable = combTable(rows, :);
+        end
+        if isempty(combTable)
+            continue
+        end
+        tableRow = combTable(1, :);
+        for iVar = 1:length(depVars)
+            depVar = depVars{iVar};
+            values = cell2mat(combTable.(depVar));
+            tableRow.(depVar) = mean(values, 'omitnan');
+            value = std(values, 'omitnan');
+            if value == 0
+                value = NaN;
             end
-            myTable = myTable(rows, :);
-        end        
-        condStrs(iStr) = strjoin(condCombs(iStr, :), " && ");
+            tableRow.([depVar, '_std']) = value;
+            tableRow.([depVar, '_n']) = length(values);
+        end
+        tableRow = removevars(tableRow, {'trial', 'side', 'fileName', 'isValid'});
+        LongTable = [LongTable; tableRow]; %#ok<AGROW>
     end
 end
 
+%% create wide mean table
+
+prepostVar = 'stage';
+prepostValues = [1 2];
+conditions = setdiff(conditions, prepostVar);
+newDepVars = depVars;
+for iVar = 1:length(depVars)
+    depVar = depVars{iVar};
+    newDepVars = [newDepVars, {[depVar, '_std'], [depVar, '_n']}]; %#ok<AGROW>
+end
+depVars = newDepVars;
+
+SourceTable = LongTable;
+WideTable = table;
+for iSubject = 1:length(subjects)
+    subject = subjects{iSubject};
+    subjectTable = SourceTable(SourceTable.subject == string(subject), :);
+    levels = cell(1, length(conditions));
+    for iCond = 1:length(conditions)
+        condition = conditions{iCond};
+        levels{iCond} = unique(subjectTable.(condition));
+    end
+    condCombs = table2cell(combinations(levels{:}));
+    nCondCombs = size(condCombs, 1);
+    for iComb = 1:nCondCombs
+        combTable = subjectTable;
+        for iCond = 1:length(condCombs(iComb, :))
+            cond = conditions{iCond};
+            value = condCombs{iComb, iCond};
+            rows = (combTable.(cond) == value);
+            combTable = combTable(rows, :);
+        end
+        if isempty(combTable)
+            continue
+        end
+        myTable = combTable(combTable.(prepostVar) == prepostValues(1), :);
+        if isempty(myTable)
+            fprintf('\t- No pre measurement for condition (%s, %s) -> skipping\n', subject, strjoin(string(condCombs(iComb, :)), ', '));
+            continue
+        end
+        myTable = combTable(combTable.(prepostVar) == prepostValues(2), :);
+        if isempty(myTable)
+            fprintf('\t- No post measurement for condition (%s, %s) -> skipping\n', subject, strjoin(string(condCombs(iComb, :)), ', '));
+            continue
+        end
+        tableRow = myTable;
+        for iVar = 1:length(depVars)
+            depVar = depVars{iVar};
+            value = combTable.(depVar)(combTable.(prepostVar) == prepostValues(1));
+            if isempty(value)
+                value = NaN;
+            end
+            tableRow.([depVar, '_pre']) = value;
+            value = combTable.(depVar)(combTable.(prepostVar) == prepostValues(2));
+            if isempty(value)
+                value = NaN;
+            end
+            tableRow.([depVar, '_post']) = value;
+            tableRow = removevars(tableRow, depVar);
+        end
+        WideTable = [WideTable; tableRow]; %#ok<AGROW>
+    end
+end
+WideTable = removevars(WideTable, {prepostVar});
+
+%% reduce tables
+
+rmVars = {'subjectCode', 'date', 'Beidbein_start', 'Beidbein_stop', 'Einbein_start', 'Einbein_stop', 'doneData', 'doneMetrics', 'donePlots'};
+% clean table
+myRmVars = intersect(CleanTable.Properties.VariableNames, rmVars);
+CleanTable = removevars(CleanTable, myRmVars);
+% long mean table
+myRmVars = intersect(LongTable.Properties.VariableNames, rmVars);
+LongTable = removevars(LongTable, myRmVars);
+% wide mean table
+myRmVars = intersect(WideTable.Properties.VariableNames, rmVars);
+WideTable = removevars(WideTable, myRmVars);
+
+%% save tables
 
 % write original table
-fprintf('\t\t- Saving Observations to table...\n');
-outpath = fullfile(outDir, 'Observations.xlsx');
-writetable(Observations, outpath, 'WriteMode', 'replacefile');
+fprintf('Saving observations to table...\n');
+saveTable(OrigTable, 'Observations_orig', {'xlsx', 'csv'}, outDir);
 
+% write clean table
+fprintf('Saving clean observations to table...\n');
+saveTable(CleanTable, 'Observations_clean', {'xlsx', 'csv'}, outDir);
 
+% write mean table
+fprintf('Saving averaged observations in long format to table...\n');
+saveTable(LongTable, 'Observations_long', {'xlsx', 'csv'}, outDir);
 
-
-
-
-
+% write wide table
+fprintf('Saving averaged observations in wide format to table...\n');
+saveTable(WideTable, 'Observations_wide', {'xlsx', 'csv'}, outDir);
 
 end
