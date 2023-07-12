c789fa0279bbc26bcc5dbda25d332d14bf3cb9c9
IVV5NET\bostroem <bostroem@uni-muenster.de>
2023-07-12 10:18:20 +0200

    loadState: import Measurements struc from base workspace, if exists

--- loadState.m
+++ loadState.m
@@ -6,8 +6,11 @@ end
 
 outDir = evalin('base', 'outDir');
 
-if isfile(fullfile(outDir, 'Measurements.mat'))
-    fprintf('Loading Measurements structure...\n');
+if evalin('base', 'exist(''Measurements'', ''var'')')
+    fprintf('Importing Measurements structure from base workspace...\n');
+    Measurements = evalin('base', 'Measurements');
+elseif isfile(fullfile(outDir, 'Measurements.mat'))
+    fprintf('Loading Measurements structure from disk...\n');
     % load Measurements structure
     tmp = load(fullfile(outDir, 'Measurements.mat'));
     Measurements = tmp.Measurements;
