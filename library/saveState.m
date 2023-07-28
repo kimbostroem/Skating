92624f160861de0f4044512697c4044bdbac17b6
IVV5NET\bostroem <bostroem@uni-muenster.de>
2023-07-28 09:52:20 +0200

    saveState: add elapsed time

--- library/saveState.m
+++ library/saveState.m
@@ -1,6 +1,8 @@
-fprintf('\t\t- Saving Measurements structure to MAT file...\n');
+fprintf('Saving Measurements structure to MAT file...\n');
+tic
 outDir = evalin('base', 'outDir');
 % export Measurements structure to base workspace
 assignin('base', 'Measurements', Measurements);
 % save Measurements structure to MAT file
 save(fullfile(outDir, 'Measurements.mat'), 'Measurements');
+fprintf('DONE in %.3f seconds\n', toc);
