bb8b25d6760f9886d761547c176cecb411287250
IVV5NET\bostroem <bostroem@uni-muenster.de>
2023-08-10 20:05:12 +0200

    Improve calculation of Balance and Sprung TargetError, remove outliers with Threshold 4

--- makeMotorMetrics.m
+++ makeMotorMetrics.m
@@ -2,11 +2,11 @@ function makeMotorMetrics()
 
 fprintf('\nMaking Motor Metrics...\n');
 
+% threshold factor to remove outliers (0 = disable, 3 = Matlab default)
+outlierThresholdFactor = 4;
+
 % load current state
 Measurements = loadState();
-
-% get outDir
-outDir = evalin('base', 'outDir');
 
 % load table containing subjects info
 SubjectsTable = Measurements.Subjects;
@@ -17,6 +17,7 @@ fprintf('Create metrics...\n');
 ticAll = tic;
 item = 1; 
 MotorMetrics = struct([]);
+estimatedBeam = 0;
 for iFile = 1:nFiles    
     fname = Measurements.MotorData(iFile).fileName;
 
@@ -133,36 +134,42 @@ for iFile = 1:nFiles
                 COPx = COP(1, :);
                 COPy = COP(2, :);
                 
-                deviation = sum(vecnorm(diff(COPy, 1, 2), 2, 1), 2, 'omitnan')/sum(diff(Time));
+                % deviation = sum(vecnorm(diff(COPy, 1, 2), 2, 1), 2, 'omitnan')/sum(diff(Time));
                 
-                % nSamples = size(COPx, 2);
-                % polyOrder = 1;
-                % coefficients = polyfit(COPx(idxContact), COPy(idxContact), polyOrder);
-                % beamYFcn = @(x) polyval(coefficients, x);
-                % % method 1 using fminsearch (faster)
-                % options = optimset('Display', 'off');
-                % ppdistanceFcn = @(x, y, x_) vecnorm([x; y] - [x_; beamYFcn(x_)]);                
-                % deviation = nan(1, nSamples);
-                % exitflag = 1;
-                % for iSample = 1:nSamples
-                %     if ~idxContact(iSample)
-                %         continue
-                %     end
-                %     minFcn = @(x_) ppdistanceFcn(COPx(iSample), COPy(iSample), x_);
-                %     [~, deviation(iSample), exitflag] = fminsearch(minFcn, COPx(iSample), options);
-                %      if exitflag <= 0
-                %          break
-                %      end
-                % end
-                % if exitflag <= 0
-                %     % method 2 using min (slower)
-                %     BeamX = linspace(min(COPx), max(COPx), nSamples);
-                %     BeamY = beamYFcn(BeamX);
-                %     distanceFcn = @(x, y) min(vecnorm([x; y] - [BeamX; BeamY]));
-                %     deviation = arrayfun(distanceFcn, COPx, COPy);
-                % end
+                % estimate beam function for calculation of distance
+                if ~estimatedBeam
+                    polyOrder = 0; % 0 = fixed distance from x-axis, 1 = line with slope
+                    coefficients = polyfit(COPx(idxContact), COPy(idxContact), polyOrder);
+                    beamYFcn = @(x) polyval(coefficients, x);
+                    % method 1 using fminsearch (faster)
+                    options = optimset('Display', 'off');
+                    ppdistanceFcn = @(x, y, x_) vecnorm([x; y] - [x_; beamYFcn(x_)]);
+                    estimatedBeam = 1; % set flag that estimation is done
+                    fprintf('Succesfully estimated beam distance at %fm\n', coefficients);
+                end
 
+                nSamples = size(COPx, 2);
+                deviation = nan(1, nSamples);
+                exitflag = 1;
+                for iSample = 1:nSamples
+                    if ~idxContact(iSample)
+                        continue
+                    end
+                    minFcn = @(x_) ppdistanceFcn(COPx(iSample), COPy(iSample), x_);
+                    [~, deviation(iSample), exitflag] = fminsearch(minFcn, COPx(iSample), options);
+                     if exitflag <= 0
+                         break
+                     end
+                end
+                if exitflag <= 0
+                    % method 2 using min (slower)
+                    BeamX = linspace(min(COPx), max(COPx), nSamples);
+                    BeamY = beamYFcn(BeamX);
+                    distanceFcn = @(x, y) min(vecnorm([x; y] - [BeamX; BeamY]));
+                    deviation = arrayfun(distanceFcn, COPx, COPy);
+                end
                 targetError = mean(deviation, 'omitnan');
+
         case 'Einbein'
                 meanCOP = mean(COP, 2, 'omitnan');
                 deviationFcn = @(x, y) vecnorm([x; y] - meanCOP);
@@ -170,11 +177,14 @@ for iFile = 1:nFiles
                 targetError = mean(deviation, 'omitnan');
             
         case 'Sprung'
+            % get jump landing point
+            landingPos = getLandingPos(Measurements.MotorData(iFile).COP);
+            if isnan(landingPos)
+                fprintf('\t\tNo landing position detected -> set targetError to NaN\n');
+            end
             % distance to jump stop position
-            deviation = abs(Measurements.MotorData(iFile).COP(1, :) - stopPos(1));
-            [targetError, targetIdx] = min(deviation);
-            jumpStopPos = Measurements.MotorData(iFile).COP(:, targetIdx);
-            Measurements.MotorData(iFile).jumpStopPos = jumpStopPos;
+            targetError = abs(landingPos(1) - stopPos(1));
+            Measurements.MotorData(iFile).landingPos = landingPos;
     end
 
     % jerk
@@ -224,8 +234,28 @@ end
 % remove subject code, as it is already encoded in subject ID and stage
 MotorMetrics = rmfield(MotorMetrics, 'SubjectCode');
 
+% convert structure array to table
+MotorMetrics = struct2table(MotorMetrics);
+
+% remove outliers
+if outlierThresholdFactor > 0
+    variables = {'PathLength', 'TargetError', 'Jerk', 'JerkXY', 'JerkZ'};
+    tasks = unique(MotorMetrics.Task);
+    for iVar = 1:length(variables)
+        variable = variables{iVar};
+        for iTask = 1:length(tasks)
+            rows = (Measurements.MotorMetrics.Task == task);
+            Data = MotorMetrics.(variable)(rows);
+            idx = isoutlier(Data, 'ThresholdFactor', outlierThresholdFactor);
+            Data(idx) = NaN;
+            MotorMetrics.(variable)(rows) = Data;
+            fprintf('Removed %d outliers from variable ''%s'' in task ''%s''\n', sum(idx), variable, task);
+        end
+    end
+end
+
 % append MotorMetrics table to Measurements structure
-Measurements.MotorMetrics = struct2table(MotorMetrics);
+Measurements.MotorMetrics = MotorMetrics;
 
 % export Measurements structure to base workspace
 fprintf('\t\t- Exporting Measurements structure to base workspace...\n');
