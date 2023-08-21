0b45bb91caeee0a867f1db047d416571da251da4
IVV5NET\bostroem <bostroem@uni-muenster.de>
2023-08-21 12:16:27 +0200

    saveState: saving now in MAT file Version v7.3 to enable saving files larger than 2 GB

--- library/saveState.m
+++ library/saveState.m
@@ -4,5 +4,5 @@ outDir = evalin('base', 'outDir');
 % export Measurements structure to base workspace
 assignin('base', 'Measurements', Measurements);
 % save Measurements structure to MAT file
-save(fullfile(outDir, 'Measurements.mat'), 'Measurements');
+save(fullfile(outDir, 'Measurements.mat'), 'Measurements', '-v7.3');
 fprintf('DONE in %.3f seconds\n', toc);
