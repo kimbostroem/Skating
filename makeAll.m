0ae6fb2fc4ed4197f404f1b92fb1f22a8c4fa3cf
IVV5NET\bostroem <bostroem@uni-muenster.de>
2023-08-21 12:10:33 +0200

    makeAll: if dataSet is 'All' -> joinData

--- makeAll.m
+++ makeAll.m
@@ -1,6 +1,6 @@
 dataSets = {
-%    'PR'
-    'PB'
+    %    'PR'
+    %    'PB'
     'PRCO'
     'ProKo'
     'All'
@@ -10,6 +10,18 @@ for iData = 1:length(dataSets)
     clear Measurements;
     close all;
     dataSet = dataSets{iData};
+
+    % if dataSet is 'All' -> join dataSets and quit
+    if strcmp(dataSet, allDir)
+        diary(fullfile(outDir,'log.txt'));
+        fprintf('Joining dataSets into ''%s''...\n', dataSet);
+        joinData();
+        diary off
+        break
+    end
+
+    % process dataSet
+    
     fprintf('Using dataset ''%s''...\n', dataSet);
     % set folders
     outDir = fullfile(parentOutDir, dataSet);
