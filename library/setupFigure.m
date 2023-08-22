2062d8ae8f8b21ae140915ecab42ad08199f30e2
IVV5NET\bostroem <bostroem@uni-muenster.de>
2023-08-22 12:49:23 +0200

    saveFigure: put figure in bottom left corner (before it was positioned outside the actual screen somehow)

--- library/setupFigure.m
+++ library/setupFigure.m
@@ -3,12 +3,12 @@ function figHandle = setupFigure(width,height,titleText,varargin)
 pause(.1);
 
 scrsz = get(0,'ScreenSize');
-figHandle = figure('Position',[1,scrsz(4),width,height],'name',titleText,'numbertitle','off',varargin{:});
+figHandle = figure('Position',[0,0,width,height],'name',titleText,'numbertitle','off',varargin{:});
 format long;
 format compact;
 set(figHandle, 'PaperType', 'A4');
 set(figHandle, 'PaperUnits', 'centimeters');
-set(figHandle,'renderer','painters');
+% set(figHandle,'renderer','painters');
 set(figHandle,'PaperPositionMode','auto');
 set(figHandle,'PaperSize',[width/72,height/72]);
 set(figHandle,'FileName',titleText);
